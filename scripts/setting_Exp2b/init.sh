099afe9c495aeaab6fee0c6110c2e79b1073e90a
Manu Wyser <manuwyser@gmail.com>
2021-10-15 02:46:05 +0200

    test bash-scripting for commit: on Fri Oct 15 02:45:53 CEST 2021

--- scripts/setting_Exp2b/init.sh
+++ scripts/setting_Exp2b/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 clear
+path="/Users/manuwyser/Dropbox/PhD_Thesis/git_local/work_mpm/C_code/scripts/setting_Exp2b"
+cd ${path}
 rm *.txt
 rm *.png
 rm ep23De_cpu
---
+++ src/commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+clear
+#------------------------------------------------------------------
+# Refs.
+# https://cmake.org/cmake/help/latest/manual/cmake.1.html
+#------------------------------------------------------------------
+path="/Users/manuwyser/Dropbox/PhD_Thesis/git_local/work_mpm/C_code"
+DATE=$(date)
+cd ${path}
+echo "Directory: "${path} 
+echo ${DATE}
+echo "Enter message for commit:" 
+read commit_message
+git add -A
+git commit -m "${commit_message}: on ${DATE}"
+git push -u origin struct
