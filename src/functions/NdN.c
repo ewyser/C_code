f9007a922063a7aa88abf466d7efb8fe37959642
Manu Wyser <manuwyser@gmail.com>
2021-10-23 21:39:52 +0200

    working bspline true: on Sat Oct 23 21:39:42 CEST 2021

--- src/functions/NdN.c
+++ src/functions/NdN.c
@@ -20,18 +20,6 @@ void NdN(DAT *N_dN, DAT xi, int type){
             N_dN[1] = -3.0/6.0*(xi*xi   )+2.0*(xi   )-2.0           ;
         }    
     }
-
-
-
-
-
-
-
-
-
-
-
-
     else if(type==2){
         if((-1.0<=xi) && (xi<=0.0)){
             N_dN[0] = -1.0/3.0*(xi*xi*xi)-1.0*(xi*xi)       +2.0/3.0;
@@ -46,20 +34,6 @@ void NdN(DAT *N_dN, DAT xi, int type){
             N_dN[1] = -3.0/6.0*(xi*xi   )+2.0*(xi   )-2.0           ;
         }    
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     else if(type==3){
         if((-2.0<=xi) && (xi<=-1.0)){
             N_dN[0] = 1.0/6.0*(xi*xi*xi)+1.0*(xi*xi)+2.0*xi+4.0/3.0;
@@ -78,21 +52,6 @@ void NdN(DAT *N_dN, DAT xi, int type){
             N_dN[1] = -3.0/6.0*(xi*xi   )+2.0*(xi   )-2.0           ;            
         }            
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     else if(type==4){
             if((-2.0<=xi) && (xi<=-1.0)){
             N_dN[0] =  1.0/6.0*(xi*xi*xi)+1.0*(xi*xi)+2.0*xi+4.0/3.0;
--- src/functions/basis.c
+++ src/functions/basis.c
@@ -12,28 +12,24 @@ void basis(mesh_t *meD, point_t *mpD){
     for(int n=0;n<nn;n++){
         for(int p=0;p<nmp;p++){
             iD  = p+n*nmp;       
-            
             // x-basis and x-derivative
             type = 0;
             xmin = meD->xB[0];
             xmax = meD->xB[1];
             type = whichType(meD->xn[mpD->p2n[iD]+0*no], xmin, xmax, meD->h[0]);
             NdN(Nx_dNx,(mpD->xp[p+0*nmp]-meD->xn[mpD->p2n[iD]+0*no])*(DAT)1.0/(DAT)meD->h[0],type);
-            
             // y-basis and y-derivative
             type = 0;
             xmin = meD->xB[2];
             xmax = meD->xB[3];
             type = whichType(meD->xn[mpD->p2n[iD]+1*no], xmin, xmax, meD->h[1]);
             NdN(Ny_dNy,(mpD->xp[p+1*nmp]-meD->xn[mpD->p2n[iD]+1*no])*(DAT)1.0/(DAT)meD->h[1],type);
-            
             // z-basis and y-derivative
             type = 0;
             xmin = meD->xB[4];
             xmax = meD->xB[5];
             type = whichType(meD->xn[mpD->p2n[iD]+2*no], xmin, xmax, meD->h[2]);   
             NdN(Nz_dNz,(mpD->xp[p+2*nmp]-meD->xn[mpD->p2n[iD]+2*no])*(DAT)1.0/(DAT)meD->h[2],type);
-            
             // convolution of basis
             mpD->N[iD]   = Nx_dNx[0]*Ny_dNy[0]*Nz_dNz[0];
             mpD->dNx[iD] = Nx_dNx[1]*Ny_dNy[0]*Nz_dNz[0];
